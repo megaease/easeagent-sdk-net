c4eff980d83c868242beb6d86282c82ff359db5c
yufu.deng <observeralone@gmail.com>
2023-01-05 18:23:15 +0800

    add Tests to build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 dotnet tool restore
 dotnet paket restore
 dotnet restore Src
+dotnet restore Tests
 dotnet restore Examples/aspnetcore/common
 dotnet restore Examples/aspnetcore/backend
 dotnet restore Examples/aspnetcore/frontend
--- source.cmd
+++ source.cmd
@@ -4,6 +4,7 @@ cls
 dotnet tool restore
 dotnet paket restore
 dotnet restore Src
+dotnet restore Tests
 dotnet restore Examples/aspnetcore/common
 dotnet restore Examples/aspnetcore/backend
 dotnet restore Examples/aspnetcore/frontend
