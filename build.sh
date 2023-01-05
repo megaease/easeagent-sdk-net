3e9b10a7d8493d3a37efd3e0b5c820a836fbaa1e
yufu.deng <observeralone@gmail.com>
2023-01-05 17:36:22 +0800

    lock 1.5.0-megaease

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 dotnet tool restore
 dotnet paket restore
+dotnet restore Src
+dotnet restore Examples/aspnetcore/common
+dotnet restore Examples/aspnetcore/backend
+dotnet restore Examples/aspnetcore/frontend
